5ab74d3d649f3e88cc3294bf6ce3eb356ee51b18
minxiaorong <minxiaorong@126.com>
2018-07-23 16:36:13 +0800

    add ignore file

---
+++ src/main/java/com/jarorwar/demo/hdfs/HdfsOperation.java
@@ -0,0 +1,10 @@
+package com.jarorwar.demo.hdfs;
+/**
+ * Copyright by 中旌影视 (c) 2018 Inc.
+ * @描述
+ * @Author Administrator
+ * @创建时间 2018/7/23/023 16:34
+ */
+public class HdfsOperation {
+    Configuration
+}
